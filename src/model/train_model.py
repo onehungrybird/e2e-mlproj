8fa20992b03d72be7361fa0a5d62f67ecd2a929e
Manish Kumar Sahu <manishkumar.sahu@adidas.com>
2024-11-02 12:13:04 +0530

    model training and prediction on test is done

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -3,7 +3,8 @@
 import pandas as pd
 import pickle
 from sklearn.linear_model import LinearRegression
-from src.data_preprocessing.data_utils import load_data, preprocess_data
+from src.data_prep.data_utils import load_data, preprocess_data
+from sklearn.model_selection import train_test_split
 
 # Load and preprocess data
 data = load_data('data/sales_data.csv')
@@ -18,12 +19,18 @@ y = data['Sales']
 # One-hot encoding for categorical features
 X = pd.get_dummies(X, columns=['Store'], drop_first=True)
 
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+
+# Save test data for later use in prediction.py
+X_test.to_csv('data/X_test.csv', index=False)
+y_test.to_csv('data/y_test.csv', index=False)
+
 # Train the model
 model = LinearRegression()
-model.fit(X, y)
+model.fit(X_train, y_train)
 
 # Save the model
-with open('models/sales_model.pkl', 'wb') as model_file:
+with open('src/model/sales_model.pkl', 'wb') as model_file:
     pickle.dump(model, model_file)
 
 print("Model trained and saved successfully.")
---
+++ src/prediction/predict.py
@@ -0,0 +1,24 @@
+# src/prediction/predict.py
+
+import pandas as pd
+import pickle
+from src.data_prep.data_utils import load_data, preprocess_data
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the trained model
+with open('src/model/sales_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Load the test data
+X_test = pd.read_csv('data/X_test.csv')
+y_test = pd.read_csv('data/y_test.csv')
+
+# Make predictions
+predictions = model.predict(X_test)
+print(predictions, '\n')
+
+# Calculate evaluation metrics
+mse = mean_squared_error(y_test, predictions)
+r2 = r2_score(y_test, predictions)
+
+print(f"Model evaluation on test data:\nMean Squared Error: {mse}\nR-squared: {r2}")
